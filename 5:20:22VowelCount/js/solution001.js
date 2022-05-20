a3735d3829c86762e63e1ab47d309f30f8c837a8
Seth Martin <jahgpup@gmail.com>
2022-05-20 10:52:02 -0500

    7kyu are a little more complicated, but this shouldnt be too hard

---
+++ 5:20:22VowelCount/js/solution001.js
@@ -0,0 +1,10 @@
+//Ok 7Kyu are a little much but this shouldn't be too hard. Let's see what I was doing wrong.
+
+function getCount(str){
+    let vowelCount = 0;
+    for(let i = 0; i < str.length; i++){
+      if(i === "a","e","i","0","u"){
+        return vowelCount;
+      }
+        }
+  }

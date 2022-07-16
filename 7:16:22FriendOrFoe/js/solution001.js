071c670acce92999b58e805d0ac2d7c572e760a6
Seth Martin <jahgpup@gmail.com>
2022-07-16 14:25:31 -0600

    now im getting it

---
+++ 7:16:22FriendOrFoe/js/solution001.js
@@ -0,0 +1,5 @@
+//Maybe my new speed record, and also some of the shortest code I've written for a solution
+
+function friend(friends){
+    return friends.filter(friend => friend.length === 4); 
+  }
---
+++ 7:9:22TwotoOne/js/solution001.js
@@ -0,0 +1,5 @@
+//Well that was hard 
+
+function longest(s1, s2){
+    return [...new Set((s1 + s2).split(''))].sort().join('');
+  }
